8dd61efaa30b906d35eb390c52e9821390ffa60b
Pierre <pierretrehin@yahoo.fr>
2021-07-20 14:29:28 +0200

    Replaced GUI with GenericDialog

--- Fiji.app/scripts/Edit/Selection/Mask_from_Rois/Binary_(0-255)_mask(s)_from_Roi(s).py
+++ Fiji.app/scripts/Edit/Selection/Mask_from_Rois/Binary_(0-255)_mask(s)_from_Roi(s).py
@@ -11,17 +11,30 @@ Overlapping ROIs will result in a single "white blob" in the mask.
 The plugin should be executed after having annotated all ROIs in an image, or all image-slices of a stack.
 The script parameters define default values via value = , to avoid having null references when a field is empty
 """
-#@ Boolean (label="Show mask(s)", value=true) show_mask
-#@ Boolean (label="Save mask(s)", value=false) save_mask
-#@ File    (label="Save mask(s) in directory", style="directory", value="") outDir
-#@ String  (label="Filename suffix (optional)", value="") suffix
-#@ String  (label="Save masks as", choices={"tif", "tiff", "png", "jpg", "gif", "bmp"}, value="tif") extension
 #@ ImagePlus imp
 #@ RoiManager rm
-from ij.gui import Overlay
+from ij.gui import Overlay, GenericDialog
+from fiji.util.gui import GenericDialogPlus
 from ij     import IJ, ImageStack, ImagePlus
 import os
 from FilenameGetter import getImageName, getSliceName
+
+gui = GenericDialog("Mask from Roi")
+gui.addCheckbox("Show mask(s)", True)
+gui.addToSameRow()
+gui.addCheckbox("Save mask(s)", False)
+gui.addDirectoryField("Save mask(s) in directory", "")
+gui.addStringField("Filename suffix (optional)", "")
+gui.addChoice("Save masks as", ["tif", "tiff", "png", "jpg", "gif", "bmp"], "tif")
+
+gui.showDialog()
+
+if gui.wasOKed():
+	show_mask = gui.getNextBoolean()
+	save_mask = gui.getNextBoolean()
+	outDir = gui.getNextString()
+	suffix = gui.getNextString()
+	extension = gui.getNextChoice()
 
 if imp.isHyperStack():
 	IJ.error("Hyperstack are not supported, use single-slider stack instead.\n" +
